ab717b307ba7566ba8e8f0613041b0f33721bae1
MiWu <1649499464@qq.com>
2022-04-18 21:30:28 +0800

    添加参数指定目录

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"flag"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
+var path *string
+
 func main() {
+	path = flag.String("path", "./", "path to the directory")
+	flag.Parse()
 	g := gin.Default()
-	g.StaticFS("/", http.Dir("./"))
+	g.StaticFS("/", http.Dir(*path))
 	g.Run(":8088")
 }
